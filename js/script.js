a5b950e957336fbef34bc2427eeb55176a1cfc52
mugeshkumar08 <mugeshkumar0809@gmail.com>
2025-01-02 14:54:07 +0530

    changes in some sections

--- js/script.js
+++ js/script.js
@@ -12,3 +12,72 @@ document.querySelector('.mobile-overlay').addEventListener('click', function() {
     document.querySelector('.mobile-menu').classList.remove('active');
     this.style.display = 'none';
 });
+
+// Our Department Section
+$(document).ready(function(){
+    $('#departmentSlider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        cssEase: 'linear',
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
+
+// slick slider for Specialists section
+$(document).ready(function(){
+    $('#specialistsSlider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        cssEase: 'linear',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
